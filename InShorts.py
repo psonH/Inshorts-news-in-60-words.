743506539fe631e34fbae60b3b93444f50fda207
Priyank Hajela <35231175+psonH@users.noreply.github.com>
2018-07-03 13:13:25 +0530

    updated to translate news from English to Hindi

--- InShorts.py
+++ InShorts.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as beauty
+from textblob import TextBlob
 import requests
 
 source = requests.get('https://inshorts.com/en/read').text
@@ -7,12 +8,12 @@ soup = beauty(source,'lxml')
 for news in soup.find_all('div',class_='news-card z-depth-1'):
     news_headline = news.find('div',class_='news-card-title news-right-box')
     news_headline = news_headline.a.span.text
-    print(news_headline)
+    trans_headline = TextBlob(news_headline)
+    print(trans_headline.translate(to='hi'))
 
     news_content = news.find('div',class_='news-card-content news-right-box')
     news_content = news_content.div.text
-    print(news_content)
-
+    trans_content = TextBlob(news_content)
+    print(trans_content.translate(to='hi'))
+    
     print()
-
-
